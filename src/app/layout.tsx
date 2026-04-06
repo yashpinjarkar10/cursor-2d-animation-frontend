e72c9b2116003746f37b006fc715823363e19c99
yashpinjarkar10 <yashpinjarkar2003@gmail.com>
2026-04-06 11:54:54 +0530

    fix: add suppressHydrationWarning and relative class to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark relative" suppressHydrationWarning>
       <body
         className={`${inter.variable} ${playfair.variable} font-sans antialiased bg-black text-white`}
       >
